c68897118b717f15e625f31965008d2bc0369dee
JojoBaPb <sipo.jmudenda@gmail.com>
2022-12-09 15:14:37 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns an alphabet in reverse
+ *
+ * Return: Alwasy 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 'z';
+	
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
