19b3e7bf1b1744b75c95285b5c717e0476373343
JojoBaPb <sipo.jmudenda@gmail.com>
2022-12-13 10:07:31 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase, followed by a new line.
+ * 
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
