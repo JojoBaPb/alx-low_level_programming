e13f8ced609c14234029b236c2912bcffd7adf98
JojoBaPb <sipo.jmudenda@gmail.com>
2022-12-15 12:14:47 -0800

    9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	int i = 0, a;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for(a = 0; a < size; a++)
+				_putchar("#");
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}				
