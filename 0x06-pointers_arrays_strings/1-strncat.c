9c62b1de98122466dcb7c48e7e5d7214bc1b65d0
JojoBaPb <sipo.jmudenda@gmail.com>
2022-12-22 15:18:50 -0800

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncat - a function that concatenates two strings.
- * @dest: string a 
+ * @dest: string a
  * @src: string b
  * @n: number of bytes to cnoncatenate with
  * Return: pointer to string
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int a, b;
 
 	for (a = 0; dest[a] != '\0'; a++)
-			;
+		;
 
 	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
 	{
