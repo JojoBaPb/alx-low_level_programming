fea2ca1b0449e7683c9bd605dc8038568341bcc9
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-02 16:52:42 -0800

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * memcpy - copy memory area
+ * @dest: char array to copy into
+ * @sec: char array to copy from
+ * @n: number of elements copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		dest[i] = src[i];
+	}
+	return (dest);
+}	
