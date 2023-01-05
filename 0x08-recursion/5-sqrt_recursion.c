6a64eaf8f37e42e8fd5e9cdb692f5f4a86560d88
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-05 02:48:39 -0800

    6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number
+ * Return: If n does not have a natural square root, 
+ * the function should return -1
+ */
+int _sqrt_recursion(int n)
+{
+	return (h1(n, 1));
+}
+
+/**
+ * h1 - finds sqr root of of numbers
+ * @a: number to find sqr root
+ * @b: to be compared with a
+ * Return: sqr root or -1
+ */
+int h1(int a, int b)
+{
+	int sqr;
+
+	sqr = b * b;
+	if (sqr == c)
+		return (b);
+	else if (sqr < a)
+		return (h1(a, b + 1));
+	else
+		return (-1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * recur - recursion loop
+ * @n: num
+ * @i: iterator
+ * Return: returns 0 or 1
+ */
+int recur(int n, int i)
+{
+	if (i == n -1)
+	{
+		return (1);
+	}
+
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+
+	if (n % i != 0)
+	{
+		return (recur(n, i + 1));
+	}
+
+	return (0);
+}
+
+/**
+ * is_prime_number - queries if prime number
+ * @n: num
+ * Return: returns 1 if prime or 0 if not.
+ */
+int is_prime_number(int num)
+{
+	int i;
+
+	i = 2;
+
+	if (num < 2)
+	{
+		return (0);
+	}
+
+	if (num == 2)
+	{
+		return (1);
+	}
+
+	return (recur(num, i));
+}
