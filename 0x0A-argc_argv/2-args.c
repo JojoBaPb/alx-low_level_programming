5d2ac74c234b6bb3f3b9610db5558c1ebd976bef
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-08 13:42:20 -0800

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments
+ * @argc: counts arguments
+ * @argv: arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char argv)
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf"%s\n", argv[a]);
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds +ve numbers
+ * @argc: counts arguments
+ * @argv: argument
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, z = 0;
+	
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (!isdigit(argv[x][y]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", z);
+
+	return (0);
+}
