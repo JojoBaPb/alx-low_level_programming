2f3988ed7f44b602f68b8a1cd5b1b711603cea4e
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-16 13:40:10 -0800

    1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,8 @@
  * init_dog -  initialize a variable of type struct dog
  *@d: dog data type
  *@name: dog name
- *age: dog age
- *@:owner dog owner
- *Return: N/a
+ *@age: dog age
+ *@owner: dog owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
