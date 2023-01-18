ff5ecf829d5f9db892e71f285b5e4dee76dbea12
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-18 12:48:19 -0800

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a fn given as para on each element of array
+ * @array: array used
+ * @size: size of array
+ * @action: pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	for (a = 0; a < size; a++)
+		action(array[a]);
+}
