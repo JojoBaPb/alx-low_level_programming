6ba04cee6f47af544e8a71a7e2b81187b0011369
JojoBaPb <sipo.jmudenda@gmail.com>
2023-01-22 12:04:20 -0800

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,18 +8,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list lists;
+	va_list list;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(lists, n);
+	va_start(list, n);
 	
 	for (i = 0; i < n; i++)
-		sum += va_arg(lists, int);
+		sum += va_arg(list, int);
 
-	va_end(lists);
+	va_end(list);
 
 	return (sum);
 }
