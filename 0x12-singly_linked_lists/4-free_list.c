e43fb21c6ce37fb39cf385bf709a3e1bedfa23a4
JojoBaPb <sipo.mudenda@gmail.com>
2023-01-26 11:58:34 -0800

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_list - free a 'list_t' list
+ * @head: first node in linked list
+ */
+void free_list(list_t *head)
+{
+	list_t *hia;
+
+	while ((hia = head) != NULL)
+	{
+		head = head->next;
+		free(hia->str);
+		free(hia)
+	}
+}
