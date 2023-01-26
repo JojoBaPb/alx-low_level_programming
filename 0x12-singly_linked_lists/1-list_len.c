c6c662c09139f72188e3596bb9f63e0032ad3dd8
JojoBaPb <sipo.mudenda@gmail.com>
2023-01-26 11:27:08 -0800

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * list_len - returns the number of elements in 'list_t' list
+ * @h: head of linked list
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t hia;
+
+	hia = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		hia++;
+	}
+
+	return (hia);
+}
