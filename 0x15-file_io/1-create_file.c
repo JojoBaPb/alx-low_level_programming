c84e4095c64cd3716dec7c0fb965c7b44d4f6a07
JojoBaPb <sipo.jmudenda@gmail.com>
2023-03-01 08:17:50 -0800

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, stat, x;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (x = 0; text_content[x] != '\0'; x++)
+			;
+		rstatus = write(fd, text_content, x);
+		if (stat == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
